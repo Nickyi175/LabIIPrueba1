f0154fe4beeff94f785720d01b0d52dcfead6bfa
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-11-03 12:14:50 -0600

    Ahorcado

---
+++ src/juegoahorcado/JuegoAhorcado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package juegoahorcado;
+
+/**
+ *
+ * @author dell
+ */
+public interface JuegoAhorcado {
+  
+    void inicializarPalabraSecreta();
+    
+    void jugar();
+}
